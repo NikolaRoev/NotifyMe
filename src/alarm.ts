33118085bfa8f3053ebef924ccc338ad951874e3
Nikola Roev <nikola.roev@gmail.com>
2024-06-24 15:25:25 +0300

    Add alarm helper functions.

--- src/alarm.ts
+++ src/alarm.ts
@@ -5,13 +5,14 @@ import { info } from "./log";
 const ALARM_NAME = "NOTIFYME-ALARM";
 
 
-export function createAlarm(periodInMinutes: number) {
-    info(`Created alarm with an update period of ${periodInMinutes} minutes.`);
+export async function createAlarm(periodInMinutes: number) {
+    await info(`Created alarm with an update period of ${periodInMinutes} minutes.`);
     return chrome.alarms.create(ALARM_NAME, { periodInMinutes });
 }
 
 export async function checkAlarm(periodInMinutes: number) {
     const alarm = await chrome.alarms.get(ALARM_NAME);
+    // FIXME: Remove when Chrome types API is up to date with documentation.
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (!alarm || alarm.periodInMinutes !== periodInMinutes) {
         await createAlarm(periodInMinutes);
